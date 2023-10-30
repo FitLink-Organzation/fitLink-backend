5775f43f027462ef2e3b8041b8909d15432bccf4
pemix09 <pemix09@onet.pl>
2023-10-30 08:40:22 +0100

    added root response

--- routing/routing.ts
+++ routing/routing.ts
@@ -15,6 +15,9 @@ export default class Routing {
     }
 
     private createRoutes(): void {
+        this._router.get('/', (req, res) => {
+            res.status(200).json('Hello world!');
+        })
         this._router.use('/auth', authRoute());
         this._router.use('/training', trainingRoute());
     }
